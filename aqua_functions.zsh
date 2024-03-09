e3f692c75185a52b7ba50f7fcc9e14323dd4e2f7
lava <zachlava1@gmail.com>
2024-03-08 22:09:22 -0600

    activate python venv

--- aqua_functions.zsh
+++ aqua_functions.zsh
@@ -10,24 +10,22 @@ pl() { tmux select-pane -L }
 hist() { peco < $HISTFILE }
 
 fzfind() { fzf --reverse --multi --preview $'{} \n stat -c %s {} | numfmt --to=iec' }
-rpi4() {
+rpi4sync() {
   sudo rsync -R $(gum file) 
     alarm@192.168.1.150:$(gum input --placeholder "Destination...") 
 }
 chmodx() { sudo chmod u+x $1 } 
-# chmodx() { 
-  # if [[ $1 ]]; then; sudo chmod u+x $1; 
-  # else; sudo chmod u+x $(gum file); fi 
-# }
+
+
+# python
+activate() { source ./bin/activate }
 
 # apps
 gs() { split_arg "git status" $1 }
 lg() { split_arg lazygit $1 }
 pacmans() { sudo pacman -S $1 }
-gn() { glow /mnt/c/Users/aquarock/share/nap/Notes }
 buku-export() { buku -e /mnt/c/Users/aquarock/share/buku_html/bookmarks.html }
 n() { nap $(nap list | peco) | glow }
-k() { kabmat }
 cht() { 
   cht.sh $(gum input --placeholder "language...") \
   $(gum input --placeholder "query...") | gum pager  
