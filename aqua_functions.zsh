ac3c742ae044cc7b8ba5f0ff6ee50096a67bb3a0
aquarock <aqua.rock.slug@gmail.com>
2024-05-28 15:18:56 -0500

    added gum to clip function

--- aqua_functions.zsh
+++ aqua_functions.zsh
@@ -1,27 +1,27 @@
 # zsh functions
 
-# tmux
 split() { tmux split-window -$1; tmux send-keys $2 Enter }
 split_arg() { if [[ $2 ]]; then; split $2 $1; else; eval $1; fi }
 pl() { tmux select-pane -L }
 
-# utilities
 hist() { peco < $HISTFILE }
 fzfind() { fzf --reverse --multi --preview $'{} \n stat -c %s {} | numfmt --to=iec' }
+docs() { ~/home/share/docs/search.sh }
 chmodx() { sudo chmod u+x $1 } 
 dl() { echo $1 >> ~/home/Downloads/dl.txt}
 clip() { 
-	CLIP=$1; START=$2; END=$3
-	DURATION=$((END-$START))
+	CLIP=$(gum file) 
+	echo Start: ; START=$(gum input --placeholder "00:00:00"); #END=$3
+	echo Duration: ; DURATION=$(gum input --placeholder "00:00:00")
+	# DURATION=$((END-$START))
 	NAME=${CLIP%.*}
-	OUTPUT=$NAME.gif
-	ffmpeg -y -i $CLIP -ss 00:00:$START -t 00:00:$DURATION -async -1 $OUTPUT
+	OUTPUT=$NAME.webm
+	ffmpeg -y -i $CLIP -ss $START -t $DURATION -async -1 $OUTPUT
 	ffmpeg_crt $OUTPUT
 	rm $OUTPUT
-	firefox file://///wsl.localhost/Arch/$PWD/${CLIP%.*}_crt.gif
+	ffmpeg -y -i ${CLIP%.*}_crt.mp4 -filter_complex "fps=12,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=32[p];[s1][p]paletteuse=dither=bayer" ${CLIP%.*}_small.gif
+
 	echo Clipped $DURATION seconds from ${CLIP%.*}
-	ffmpeg -y -i ${CLIP%.*}_crt.gif -filter_complex "fps=12,scale=480:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=32[p];[s1][p]paletteuse=dither=bayer" ${CLIP%.*}_small.gif
-	firefox file://///wsl.localhost/Arch/$PWD/${CLIP%.*}_small.gif
 }
 activate() { source ./bin/activate }
 
