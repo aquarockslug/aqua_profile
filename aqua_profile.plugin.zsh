c622b183f1979abc9bcfce4e19add3e4ccca7eb3
aquarock <aqua.rock.slug@gmail.com>
2024-06-04 18:27:26 -0500

    select window

--- aqua_profile.plugin.zsh
+++ aqua_profile.plugin.zsh
@@ -7,10 +7,12 @@ export MANPATH="/usr/local/man:$MANPATH"
 export DISABLE_AUTO_TITLE='true'
 
 # apps
+alias p="python"
 alias py="python"
 alias s="sudo"
 alias b="buku --suggest"
 alias fx="felix"
+alias open="wsl-open"
 
 # nvim
 alias v="nvim"
@@ -30,17 +32,16 @@ alias cls="clear && ls"
 
 bindkey ' ' magic-space
 
-
-
 # tmux
 tmux set -g prefix C-p
 tmux bind -n ^h select-pane -L
 tmux bind -n ^j select-pane -D
 tmux bind -n ^k select-pane -U
 tmux bind -n ^l select-pane -R
-tmux bind -n C-w kill-pane # ^w doesn't work
+tmux bind -n C-w kill-pane # ^w doesn't work?
 tmux bind -T prefix P split-window -v -c "#{pane_current_path}"
 tmux bind -T prefix p split-window -h -c "#{pane_current_path}"
+tmux bind -T prefix h select-window -t -1
+tmux bind -T prefix l select-window -t +1
 tmux set -g mouse on
-tmux set history-limit 100000
 if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then tmux; fi
